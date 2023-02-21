4ff65d72eccd617070b55352044571d763c28bd1
Kevin Lin <kevinlin.vxd@gmail.com>
2023-02-21 00:20:08 -0600

    Fix IP address for all, add base case for spectre attack in

---
+++ Kevin-Exp/show_ml_data.m
@@ -0,0 +1,20 @@
+function []=show_ml_data(arg1)
+
+clc;
+f = readmatrix(arg1);
+mean_val = mean(f, "all");
+for i = 1 : height(f)
+    for j = 1 : length(f)
+        if (f(i,j) > 10000)
+            f(i,j) = mean_val;
+        end
+    end
+end
+M = mean(f);
+
+subplot(1,1,1)
+plot(M);
+hold on
+title('Mean Data');
+
+end
